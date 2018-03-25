507bca90e632362b740cd0bf31d4989507ed4207
Unknown <segler_alex@web.de>
2018-03-25 22:53:35 +0200

    content type split on ;

--- src/streamcheck.rs
+++ src/streamcheck.rs
@@ -6,7 +6,7 @@ use playlist_decoder;
 use url::Url;
 
 #[derive(Debug)]
-pub struct StreamInfo{
+pub struct StreamInfo {
     pub Name: String,
     pub Description: String,
     pub Type: String,
@@ -69,7 +69,16 @@ fn type_is_xspf(content_type: &str) -> bool {
 }
 
 fn type_is_playlist(content_type: &str) -> bool {
-    return type_is_m3u(content_type) || type_is_pls(content_type) || type_is_asx(content_type) || type_is_xspf(content_type);
+    let search = content_type.find(";");
+    let mut content_type = content_type;
+    match search {
+        Some(index) => {
+            content_type = &content_type[0..index];
+        }
+        None => {}
+    }
+    return type_is_m3u(content_type) || type_is_pls(content_type) || type_is_asx(content_type)
+        || type_is_xspf(content_type);
 }
 
 fn type_is_stream(content_type: &str) -> Option<&str> {
@@ -84,7 +93,7 @@ fn type_is_stream(content_type: &str) -> Option<&str> {
         "audio/flac" => Some("FLAC"),
         "application/flv" => Some("FLV"),
         "application/octet-stream" => Some("UNKNOWN"),
-        _ => None
+        _ => None,
     }
 }
 
@@ -104,35 +113,60 @@ pub fn check(url: &str) -> Vec<BoxResult<StreamInfo>> {
                             if type_is_playlist(content_type) {
                                 is_playlist = true;
                             } else if type_is_stream(content_type).is_some() {
-                                stream_type = String::from(type_is_stream(content_type).unwrap_or(""));
+                                stream_type =
+                                    String::from(type_is_stream(content_type).unwrap_or(""));
                                 is_stream = true;
-                            }else{
-                                list.push(Err(Box::new(StreamCheckError::new(url, &format!("unknown content type {}", content_type)))));
+                            } else {
+                                list.push(Err(Box::new(StreamCheckError::new(
+                                    url,
+                                    &format!("unknown content type {}", content_type),
+                                ))));
                             }
                         }
                         None => {
-                            list.push(Err(Box::new(StreamCheckError::new(url, "Missing content-type in http header"))));
+                            list.push(Err(Box::new(StreamCheckError::new(
+                                url,
+                                "Missing content-type in http header",
+                            ))));
                         }
                     }
                 }
                 if is_playlist {
                     request.read_content();
                     let playlist = decode_playlist(url, request.get_content());
-                    if playlist.len() == 0{
+                    if playlist.len() == 0 {
                         list.push(Err(Box::new(StreamCheckError::new(url, "Empty playlist"))));
                     } else {
                         list.extend(playlist);
                     }
-                }else if is_stream {
-                    let stream = StreamInfo{
+                } else if is_stream {
+                    let headers = request.info.headers;
+                    let stream = StreamInfo {
                         Url: String::from(url),
-                        Type: request.info.headers.get("content-type").unwrap_or(&String::from("")).clone(),
-                        Name: request.info.headers.get("icy-name").unwrap_or(&String::from("")).clone(),
-                        Description: request.info.headers.get("icy-description").unwrap_or(&String::from("")).clone(),
-                        Homepage: request.info.headers.get("icy-url").unwrap_or(&String::from("")).clone(),
-                        Bitrate: request.info.headers.get("icy-br").unwrap_or(&String::from("")).parse().unwrap_or(0),
-                        Genre: request.info.headers.get("icy-genre").unwrap_or(&String::from("")).clone(),
-                        Sampling: request.info.headers.get("icy-sr").unwrap_or(&String::from("")).parse().unwrap_or(0),
+                        Type: headers
+                            .get("content-type")
+                            .unwrap_or(&String::from(""))
+                            .clone(),
+                        Name: headers.get("icy-name").unwrap_or(&String::from("")).clone(),
+                        Description: headers
+                            .get("icy-description")
+                            .unwrap_or(&String::from(""))
+                            .clone(),
+                        Homepage: headers.get("icy-url").unwrap_or(&String::from("")).clone(),
+                        Bitrate: headers
+                            .get("icy-br")
+                            .unwrap_or(&String::from(""))
+                            .parse()
+                            .unwrap_or(0),
+                        Genre: headers
+                            .get("icy-genre")
+                            .unwrap_or(&String::from(""))
+                            .clone(),
+                        Sampling: headers
+                            .get("icy-sr")
+                            .unwrap_or(&String::from(""))
+                            .parse()
+                            .unwrap_or(0),
                         Codec: stream_type,
                     };
                     list.push(Ok(stream));
@@ -146,37 +180,45 @@ pub fn check(url: &str) -> Vec<BoxResult<StreamInfo>> {
                     None => {}
                 }
             } else {
-                list.push(Err(Box::new(StreamCheckError::new(url, &format!("illegal http status code {}", request.info.code)))));
+                list.push(Err(Box::new(StreamCheckError::new(
+                    url,
+                    &format!("illegal http status code {}", request.info.code),
+                ))));
             }
         }
-        Err(err) => list.push(Err(Box::new(StreamCheckError::new(url,&err.to_string())))),
+        Err(err) => list.push(Err(Box::new(StreamCheckError::new(url, &err.to_string())))),
     }
     list
 }
 
-fn decode_playlist(url_str:&str, content: &str) -> Vec<BoxResult<StreamInfo>> {
+fn decode_playlist(url_str: &str, content: &str) -> Vec<BoxResult<StreamInfo>> {
     let mut list = vec![];
     let base_url = Url::parse(url_str);
-    match base_url{
+    match base_url {
         Ok(base_url) => {
             let urls = playlist_decoder::decode(content);
-            for url in urls{
+            for url in urls {
                 let abs_url = base_url.join(&url);
-                match abs_url{
-                    Ok(abs_url)=>{
-                        println!("base='{}' + src='{}' = '{}'", base_url, url, abs_url);
+                match abs_url {
+                    Ok(abs_url) => {
                         list.extend(check(&abs_url.as_str()));
                     }
-                    Err(err)=>{
-                        list.push(Err(Box::new(StreamCheckError::new(url_str,&err.to_string()))));
+                    Err(err) => {
+                        list.push(Err(Box::new(StreamCheckError::new(
+                            url_str,
+                            &err.to_string(),
+                        ))));
                     }
                 }
             }
         }
         Err(err) => {
-            list.push(Err(Box::new(StreamCheckError::new(url_str,&err.to_string()))));
+            list.push(Err(Box::new(StreamCheckError::new(
+                url_str,
+                &err.to_string(),
+            ))));
         }
     }
-    
+
     list
 }
