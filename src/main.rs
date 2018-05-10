3f94f85f7507b47cee7dea8142ebfaea736b7d2d
Unknown <segler_alex@web.de>
2018-05-10 17:37:58 +0200

    use clap library for parameter support

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
+extern crate av_stream_info_rust;
+#[macro_use]
+extern crate clap;
 extern crate hostname;
 extern crate mysql;
 extern crate native_tls;
-extern crate av_stream_info_rust;
 extern crate threadpool;
 extern crate url;
 
-use std::env;
+use clap::{App, Arg};
 
 pub mod models;
 
@@ -25,13 +27,6 @@ use models::StationCheckItemNew;
 extern crate colored;
 
 use colored::*;
-
-fn debugcheck(url: &str, timeout: u32) {
-    let items = av_stream_info_rust::check(&url, timeout, 3, 3);
-    for item in items {
-        println!("{:?}", item);
-    }
-}
 
 fn check_for_change(old: &models::StationItem, new: &StationCheckItemNew) -> (bool, String) {
     let mut retval = false;
@@ -81,12 +76,16 @@ fn check_for_change(old: &models::StationItem, new: &StationCheckItemNew) -> (bo
     }
 }
 
-fn update_station(conn: &mysql::Pool, old: &models::StationItem, new_item: &StationCheckItemNew) {
+fn update_station(conn: &mysql::Pool, old: &models::StationItem, new_item: &StationCheckItemNew, verbosity: u8) {
     db::insert_check(&conn, &new_item);
     db::update_station(&conn, &new_item);
     let (changed, change_str) = check_for_change(&old, &new_item);
     if changed {
         println!("{}", change_str.red());
+    }else{
+        if verbosity > 0 {
+            println!("{}", change_str.dimmed());
+        }
     }
 }
 
@@ -98,6 +97,7 @@ fn dbcheck(
     timeout: u32,
     max_depth: u8,
     retries: u8,
+    verbosity: u8,
 ) -> u32 {
     let conn = db::new(connection_str);
     let mut checked_count = 0;
@@ -164,7 +164,7 @@ fn dbcheck(
                     }
                 }
 
-                update_station(&conn, &station, &new_item);
+                update_station(&conn, &station, &new_item, verbosity);
             });
         }
         pool.join();
@@ -173,41 +173,178 @@ fn dbcheck(
 }
 
 fn main() {
-    let concurrency: usize = env::var("CONCURRENCY")
-        .unwrap_or(String::from("10"))
+    let hostname: String = get_hostname().unwrap_or("".to_string());
+    let matches = App::new("stream-check")
+        .version(crate_version!())
+        .author("segler_alex@web.de")
+        .about("Stream check tool for radiobrowser")
+/*        .arg(
+            Arg::with_name("config")
+                .short("c")
+                .long("config")
+                .value_name("FILE")
+                .default_value("/etc/radiobrowser.conf")
+                .help("Sets a custom config file")
+                .takes_value(true),
+        )*/
+        .arg(
+            Arg::with_name("database")
+                .short("d")
+                .long("database")
+                .value_name("DATABASE_URL")
+                .help("Database connection url")
+                .env("DATABASE_URL")
+                .required(true)
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("source")
+                .short("s")
+                .long("source")
+                .value_name("SOURCE")
+                .help("Source string for database check entries")
+                .env("SOURCE")
+                .default_value(&hostname)
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("retries")
+                .short("r")
+                .long("retries")
+                .value_name("RETRIES")
+                .help("Max number of retries for station checks")
+                .env("RETRIES")
+                .default_value("5")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("max_depth")
+                .short("m")
+                .long("max_depth")
+                .value_name("MAX_DEPTH")
+                .help("max recursive link check depth")
+                .env("MAX_DEPTH")
+                .default_value("5")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("tcp_timeout")
+                .short("t")
+                .long("tcp_timeout")
+                .value_name("TCP_TIMEOUT")
+                .help("tcp connect/read timeout in seconds")
+                .env("TCP_TIMEOUT")
+                .default_value("10")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("pause_seconds")
+                .short("a")
+                .long("pause_seconds")
+                .value_name("PAUSE_SECONDS")
+                .help("database check pauses in seconds")
+                .env("PAUSE_SECONDS")
+                .default_value("10")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("stations")
+                .short("n")
+                .long("stations")
+                .value_name("STATIONS")
+                .help("batch size for station checks")
+                .env("STATIONS")
+                .default_value("50")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("concurrency")
+                .short("c")
+                .long("concurrency")
+                .value_name("CONCURRENCY")
+                .help("streams checked in parallel")
+                .env("CONCURRENCY")
+                .default_value("10")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("delete")
+                .short("x")
+                .long("delete")
+                .value_name("DELETE")
+                .help("delete broken stations according to rules")
+                .env("DELETE")
+                .default_value("false")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("loop")
+                .short("l")
+                .long("loop")
+                .value_name("LOOP")
+                .help("do loop checks forever")
+                .env("LOOP")
+                .default_value("false")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("v")
+                .short("v")
+                .multiple(true)
+                .help("Sets the level of verbosity"),
+        )
+        .get_matches();
+
+    // Gets a value for config if supplied by user, or defaults to "default.conf"
+    /*if let Some(config) = matches.value_of("config"){
+        println!("found config at {}", config);
+    }*/
+
+    // Vary the output based on how many times the user used the "verbose" flag
+    // (i.e. 'myprog -v -v -v' or 'myprog -vvv' vs 'myprog -v'
+    let verbosity: u8 = matches.occurrences_of("v") as u8;
+    let concurrency: usize = matches
+        .value_of("concurrency")
+        .unwrap()
         .parse()
-        .expect("CONCURRENCY is not number");
-    let check_stations: u32 = env::var("STATIONS")
-        .unwrap_or(String::from("50"))
+        .expect("concurrency is not usize");
+    let check_stations: u32 = matches
+        .value_of("stations")
+        .unwrap()
         .parse()
-        .expect("CONCURRENCY is not number");
-    let do_loop: bool = env::var("LOOP")
-        .unwrap_or(String::from("false"))
+        .expect("stations is not u32");
+    let do_loop: bool = matches
+        .value_of("loop")
+        .unwrap()
         .parse()
-        .expect("LOOP is not bool");
-    let delete: bool = env::var("DELETE")
-        .unwrap_or(String::from("false"))
+        .expect("loop is not bool");
+    let delete: bool = matches
+        .value_of("delete")
+        .unwrap()
         .parse()
-        .expect("DELETE is not bool");
-    let pause_seconds: u64 = env::var("PAUSE_SECONDS")
-        .unwrap_or(String::from("10"))
+        .expect("delete is not bool");
+    let pause_seconds: u64 = matches
+        .value_of("tcp_timeout")
+        .unwrap()
         .parse()
-        .expect("PAUSE_SECONDS is not u64");
-    let tcp_timeout: u32 = env::var("TCP_TIMEOUT")
-        .unwrap_or(String::from("10"))
+        .expect("pause_seconds is not u64");
+    let tcp_timeout: u32 = matches
+        .value_of("tcp_timeout")
+        .unwrap()
         .parse()
-        .expect("TCP_TIMEOUT is not u32");
-    let max_depth: u8 = env::var("MAX_DEPTH")
-        .unwrap_or(String::from("5"))
+        .expect("tcp_timeout is not u32");
+    let max_depth: u8 = matches
+        .value_of("max_depth")
+        .unwrap()
         .parse()
-        .expect("MAX_DEPTH is not u8");
-    let retries: u8 = env::var("RETRIES")
-        .unwrap_or(String::from("5"))
+        .expect("max_depth is not u8");
+    let retries: u8 = matches
+        .value_of("retries")
+        .unwrap()
         .parse()
-        .expect("RETRIES is not u8");
-    let source: String =
-        env::var("SOURCE").unwrap_or(String::from(get_hostname().unwrap_or("".to_string())));
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set");
+        .expect("retries is not u8");
+    let source: String = String::from(matches.value_of("source").unwrap());
+    let database_url = String::from(matches.value_of("database").unwrap());
 
     println!("DATABASE_URL  : {}", database_url);
     println!("LOOP          : {}", do_loop);
@@ -222,57 +359,52 @@ fn main() {
 
     let database_url2 = database_url.clone();
     let source2 = source.clone();
-    thread::spawn(move||{
-        loop{
-            let conn = db::new(&database_url2);
-            match conn {
-                Ok(conn) => {
-                    let checks_hour = db::get_checks(&conn, 1, &source2);
-                    let checks_day = db::get_checks(&conn, 24, &source2);
-                    let stations_broken = db::get_station_count_broken(&conn);
-                    let stations_working = db::get_station_count_working(&conn);
-                    let stations_todo = db::get_station_count_todo(&conn, 24);
-                    let stations_deletable_never_worked = db::get_deletable_never_working(&conn, 24 * 3);
-                    let stations_deletable_were_working = db::get_deletable_were_working(&conn, 24 * 30);
-                    if delete {
-                        db::delete_never_working(&conn, 24 * 3);
-                        db::delete_were_working(&conn, 24 * 30);
-                    }
+    thread::spawn(move || loop {
+        let conn = db::new(&database_url2);
+        match conn {
+            Ok(conn) => {
+                let checks_hour = db::get_checks(&conn, 1, &source2);
+                let checks_day = db::get_checks(&conn, 24, &source2);
+                let stations_broken = db::get_station_count_broken(&conn);
+                let stations_working = db::get_station_count_working(&conn);
+                let stations_todo = db::get_station_count_todo(&conn, 24);
+                let stations_deletable_never_worked =
+                    db::get_deletable_never_working(&conn, 24 * 3);
+                let stations_deletable_were_working =
+                    db::get_deletable_were_working(&conn, 24 * 30);
+                if delete {
+                    db::delete_never_working(&conn, 24 * 3);
+                    db::delete_were_working(&conn, 24 * 30);
+                }
 
-                    println!("STATS: {} Checks/Hour, {} Checks/Day, {} Working stations, {} Broken stations, {} to do, deletable {} + {}", checks_hour, checks_day, stations_working, stations_broken, stations_todo, stations_deletable_never_worked, stations_deletable_were_working);
-                    thread::sleep(Duration::from_secs(3600));
-                }
-                _ => {
-                    println!("Connection failed");
-                }
+                println!("STATS: {} Checks/Hour, {} Checks/Day, {} Working stations, {} Broken stations, {} to do, deletable {} + {}", checks_hour, checks_day, stations_working, stations_broken, stations_todo, stations_deletable_never_worked, stations_deletable_were_working);
+            }
+            _ => {
+                println!("Connection failed");
             }
         }
+        thread::sleep(Duration::from_secs(3600));
     });
 
     loop {
-        let mut checked_count = 0;
-        match env::args().nth(1) {
-            Some(url) => {
-                debugcheck(&url, tcp_timeout);
-            }
-            None => {
-                checked_count = dbcheck(
-                    &database_url,
-                    &source,
-                    concurrency,
-                    check_stations,
-                    tcp_timeout,
-                    max_depth,
-                    retries,
-                );
-            }
-        };
+        let checked_count = dbcheck(
+            &database_url,
+            &source,
+            concurrency,
+            check_stations,
+            tcp_timeout,
+            max_depth,
+            retries,
+            verbosity,
+        );
         if !do_loop {
             break;
         }
 
         if checked_count == 0 {
             thread::sleep(Duration::from_secs(pause_seconds));
+        } else {
+            thread::sleep(Duration::from_secs(1));
         }
     }
 }
