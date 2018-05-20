d7d291bf0feff62f944a3fc653527d5a0a15a602
segler <segler_alex@web.de>
2018-05-20 15:05:50 +0200

    split up code

--- src/main.rs
+++ src/main.rs
@@ -8,188 +8,19 @@ extern crate threadpool;
 extern crate url;
 extern crate website_icon_extract;
 extern crate reqwest;
+extern crate colored;
 
 use clap::{App, Arg};
 
 pub mod models;
 
-use threadpool::ThreadPool;
-
 mod db;
 mod favicon;
+mod check;
 
 use std::time::Duration;
 use hostname::get_hostname;
 use std::thread;
-use std::sync::mpsc::{Receiver, Sender};
-use std::sync::mpsc::channel;
-use std::sync::mpsc::TryRecvError;
-
-use models::StationCheckItemNew;
-
-extern crate colored;
-
-use colored::*;
-
-fn check_for_change(old: &models::StationItem, new: &StationCheckItemNew, new_favicon: &str) -> (bool, String) {
-    let mut retval = false;
-    let mut result = String::from("");
-
-    if old.check_ok != new.check_ok {
-        if new.check_ok {
-            result.push('+');
-            result.red();
-        } else {
-            result.push('-');
-        }
-        retval = true;
-    } else {
-        result.push('~');
-    }
-    result.push(' ');
-    result.push('\'');
-    result.push_str(&old.name);
-    result.push('\'');
-    result.push(' ');
-    result.push_str(&old.url);
-    if old.hls != new.hls {
-        result.push_str(&format!(" hls:{}->{}", old.hls, new.hls));
-        retval = true;
-    }
-    if old.bitrate != new.bitrate {
-        result.push_str(&format!(" bitrate:{}->{}", old.bitrate, new.bitrate));
-        retval = true;
-    }
-    if old.codec != new.codec {
-        result.push_str(&format!(" codec:{}->{}", old.codec, new.codec));
-        retval = true;
-    }
-    /*if old.urlcache != new.url{
-        println!("  url      :{}->{}",old.urlcache,new.url);
-        retval = true;
-    }*/
-    if old.favicon != new_favicon {
-        result.push_str(&format!(" favicon: {} -> {}", old.favicon, new_favicon));
-        retval = true;
-    }
-    if old.check_ok != new.check_ok {
-        if new.check_ok {
-            return (retval, result.green().to_string());
-        } else {
-            return (retval, result.red().to_string());
-        }
-    } else {
-        return (retval, result.yellow().to_string());
-    }
-}
-
-fn update_station(conn: &mysql::Pool, old: &models::StationItem, new_item: &StationCheckItemNew, new_favicon: &str, verbosity: u8) {
-    db::insert_check(&conn, &new_item);
-    db::update_station(&conn, &new_item);
-    let (changed, change_str) = check_for_change(&old, &new_item, new_favicon);
-    if changed {
-        println!("{}", change_str.red());
-    }else{
-        if verbosity > 0 {
-            println!("{}", change_str.dimmed());
-        }
-    }
-}
-
-fn dbcheck(
-    connection_str: &str,
-    source: &str,
-    concurrency: usize,
-    stations_count: u32,
-    useragent: &str,
-    timeout: u32,
-    max_depth: u8,
-    retries: u8,
-    favicon_checks: bool,
-    verbosity: u8,
-) -> u32 {
-    let conn = db::new(connection_str);
-    let mut checked_count = 0;
-    match conn {
-        Ok(conn) =>{
-            let stations = db::get_stations_to_check(&conn, 24, stations_count);
-
-            let pool = ThreadPool::new(concurrency);
-            for station in stations {
-                checked_count = checked_count + 1;
-                let source = String::from(source);
-                let useragent = String::from(useragent);
-                let conn = conn.clone();
-                pool.execute(move || {
-                    let (_, receiver): (Sender<i32>, Receiver<i32>) = channel();
-                    let station_name = station.name.clone();
-                    let max_timeout = (retries as u32) * timeout * 2;
-                    thread::spawn(move || {
-                        for _ in 0..max_timeout {
-                            thread::sleep(Duration::from_secs(1));
-                            let o = receiver.try_recv();
-                            match o {
-                                Ok(_) => {
-                                    return;
-                                }
-                                Err(value) => match value {
-                                    TryRecvError::Empty => {}
-                                    TryRecvError::Disconnected => {
-                                        return;
-                                    }
-                                },
-                            }
-                        }
-                        println!("Still not finished: {}", station_name);
-                        std::process::exit(0x0100);
-                    });
-                    let mut new_item: StationCheckItemNew = StationCheckItemNew {
-                        station_uuid: station.uuid.clone(),
-                        source: source.clone(),
-                        codec: "".to_string(),
-                        bitrate: 0,
-                        hls: false,
-                        check_ok: false,
-                        url: "".to_string(),
-                    };
-                    let items = av_stream_info_rust::check(&station.url, timeout, max_depth, retries);
-                    for item in items.iter() {
-                        match item {
-                            &Ok(ref item) => {
-                                let mut codec = item.CodecAudio.clone();
-                                if let Some(ref video) = item.CodecVideo {
-                                    codec.push_str(",");
-                                    codec.push_str(&video);
-                                }
-                                new_item = StationCheckItemNew {
-                                    station_uuid: station.uuid.clone(),
-                                    source: source.clone(),
-                                    codec: codec,
-                                    bitrate: item.Bitrate as i32,
-                                    hls: item.Hls,
-                                    check_ok: true,
-                                    url: item.Url.clone(),
-                                };
-                            }
-                            &Err(_) => {}
-                        }
-                    }
-                    if favicon_checks {
-                        let new_favicon = favicon::check(&station.homepage, &station.favicon, verbosity, &useragent, timeout);
-                        update_station(&conn, &station, &new_item, &new_favicon, verbosity);
-                    }else{
-                        update_station(&conn, &station, &new_item, &station.favicon, verbosity);
-                    }
-                });
-            }
-            pool.join();
-        }
-        Err(e)=>{
-            println!("Database connection error {}", e);
-        }
-    }
-    checked_count
-}
 
 fn main() {
     let hostname: String = get_hostname().unwrap_or("".to_string());
@@ -438,7 +269,7 @@ fn main() {
             println!("new batch");
         }
 
-        let checked_count = dbcheck(
+        let checked_count = check::dbcheck(
             &database_url,
             &source,
             concurrency,
